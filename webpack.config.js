8aecfd74d018003a1c8f7c54eb7cbb56cb1f5943
timur <t.togyzbaev@dev.userstory.ru>
2018-05-09 11:21:11 +0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: './src/js/main/index.js',
+        common: './src/js/common/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'www'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./html/index.html",
+            filename: "./index.html",
+        }),
+        new HtmlWebPackPlugin({
+            filename: "./music.html",
+            template: "./html/music.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
